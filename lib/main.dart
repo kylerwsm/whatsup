8b79b24463d5415edcd9fe50a579b10ba12b2370
Kyler Wong <wsm.kyler@gmail.com>
2020-03-14 12:37:43 +0800

    misc changes to name and configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_whatsapp/constants.dart';
 
 void main() => runApp(MyApp());
@@ -6,6 +7,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /// Forces the app to only be in portrait view.
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: AppConstants.appName,
